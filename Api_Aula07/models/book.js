4e5fad9dbfd88f4886401215814a7a35bf5e9030
Leonardordss <lrodrigues.md@gmail.com>
2024-09-11 22:56:32 -0300

    Criando API

---
+++ Api_Aula07/models/book.js
@@ -0,0 +1,5 @@
+// arquivo onde sera feita a conexao com o banco de dados
+// modelo de dados em memoria
+let books = [];
+
+module.exports = books;
--- Aula07_BKEND/Exemplos/exe1.js
+++ Aula07_BKEND/Exemplos/exe1.js
@@ -1,1 +1,23 @@
-//
+const express = require('express'); // Variável express que vai salvar uma requisição
+const app = express(); // Variável app armazena o express
+const port = 3000; // Porta padrão para as aplicações em Node
+
+// Cria uma requisição GET
+app.get('/get', (req, res) => {
+    res.send('Backend testes');
+});
+
+app.get('/usuario', (req, res) => {
+    res.send('Usuario admin');
+});
+
+// app post é para fazer um post na api com o express
+app.post('/',(req,res)=>{
+    res.send('Test post');
+});
+
+// Faz o servidor ouvir na porta especificada
+app.listen(port, () => {
+    console.log(`Exemplo app ouvindo na porta ${port}`);
+});
+
